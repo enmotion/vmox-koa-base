94ddac22c689b215b21cac423261dff598791b7f
enmotion <40427163@qq.com>
2025-04-16 12:48:11 +0800

    tested pm2 watch

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@
  * @ Author: Your name
  * @ Create Time: 2025-04-15 16:30:41
  * @ Modified by: Your name
- * @ Modified time: 2025-04-16 12:07:21
+ * @ Modified time: 2025-04-16 12:47:26
  * @ Description:
  */
 import Koa from 'koa';  // 引入 Koa 框架
 const app = new Koa();  // 创建 Koa 实例
 // 简单的中间件示例
 app.use(async (ctx) => {
-  ctx.body = 'Hello, Koa with TypeScript!';
+  ctx.body = 'Hello, Koa with TypeScript12!';
 });
 export default app; // 导出 Koa 实例
