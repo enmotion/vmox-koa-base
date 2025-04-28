6a58b716b1df541d59e1756572507fa31b9ea73e
enmotion <40427163@qq.com>
2025-04-28 09:26:04 +0800

    s

--- src/models/users/index.ts
+++ src/models/users/index.ts
@@ -14,10 +14,14 @@ import { useUserRouter } from './core/routers';
  * schema => service => controllers => router
  */
 export function userUserModel(mongoose:Mongoose,prefix:string='/users'){
-  const userModel = mongoose.model<IUser>('user',userSchema) // 获取数据查询模型
-  const userService = useUserService(userModel) // 注入查询模型，创建 service
-  const controller = useUserController(userService); // 注入 service 创建 controllers
-  const router = useUserRouter('/users',controller) // 注入 controller 创建实例
+  // 获取数据查询模型
+  const userModel = mongoose.model<IUser>('user',userSchema) 
+  // 注入查询模型，创建 service
+  const userService = useUserService(userModel) 
+  // 注入 service 创建 controllers
+  const controller = useUserController(userService); 
+  // 注入 controller 创建实例
+  const router = useUserRouter('/users',controller) 
   return {
     userModel,
     userService,
