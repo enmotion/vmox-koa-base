03ea1b8fb37265375cee991c13713b8a558b72ff
enmotion <40427163@qq.com>
2025-04-29 16:08:47 +0800

    d

--- src/use.lib/serviceTools.ts
+++ src/use.lib/serviceTools.ts
@@ -6,14 +6,14 @@ import type { Schema } from "mongoose";
  */
 export function mongoDBErrorTransform(err:any,schema:Schema){
   // 获取错误名称
-  const error: Record<string,any> = {errorName:Object(err).name}
+  const error: Record<string,any> = {name:Object(err).name}
   // 数据库错误
-  if(error.errorName === 'MongoServerError'){
+  if(error.name === 'MongoServerError'){
     // 数据库错误在此处理
     error['key'] = Object.keys(err.keyPattern)[0]
     error['options'] = schema.path(error['key']).options;
   }
-  if(error.errorName === 'ValidationError'){
+  if(error.name === 'ValidationError'){
     // 数据验证错误
     error['key'] = Object.keys(err.errors)[0];
     error['options'] = schema.path(error['key']).options;
