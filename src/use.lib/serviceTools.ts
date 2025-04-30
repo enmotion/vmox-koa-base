e8808aa3e8194064e18593e2905a5a0932c9cf6a
enmotion <40427163@qq.com>
2025-04-30 10:44:28 +0800

    update

--- src/use.lib/serviceTools.ts
+++ src/use.lib/serviceTools.ts
@@ -11,21 +11,18 @@ export function mongoDBErrorTransform(err:any,schema:Schema){
     // // 数据库错误
     if(['MongoServerError','MongooseError'].includes(err.name)){
       // 数据库错误在此处理, 两个错误类型的返回结构不太一样 MongooseError 将错误信息 封装在了 cause 对象里
-      error['key'] = Object.keys(err.cause?.keyPattern ?? err.keyPattern)[0]
+      error['key'] = Object.keys(err?.cause?.keyPattern ?? err?.keyPattern)?.[0]
       error['options'] = schema.path(error['key']).options; // 获取字段的 schema 配置
-      error['message'] = String(err).split(":")[1].replace(/\s/g,'')
-      console.log(error)
+      error['message'] = String(err).split(":")[1].replace(/\s/g,'');
     }
     if(['ValidationError'].includes(error.name)){
       // 数据验证错误
-      console.log(err)
-      error['key'] = Object.keys(err.errors)[0];
+      error['key'] = Object.keys(err?.errors)?.[0];
       error['options'] = schema.path(error['key']).options; // 获取字段的 schema 配置
-      error['message'] = String(err).split(":")[1].replace(/\s/g,'')
+      error['message'] = String(err).split(":")[1].replace(/\s/g,'');
     }
     return error;
   }catch(error){
     throw error
   }
-  
 }
