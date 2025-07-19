8b2ea7b9dc73279518d4f0324cb816e381091695
enmotion <40427163@qq.com>
2025-07-19 23:01:51 +0800

    完善问题数据结构

--- src/modules/problems/index.ts
+++ src/modules/problems/index.ts
@@ -41,7 +41,6 @@ export const problemSchema = new mongoose.Schema<ExpandProblem>(
 
 // 创建复合索引优化查询性能
 problemSchema.index({ category: 1, difficultyLevel: 1, gradeLevel: 1, status: 1, super: 1,tags: 1, });
-problemSchema.index({key:1}, {unique:true,sparse: true })
 
 /**
  * Mongoose问题集模型
